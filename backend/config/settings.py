12b44d2fbbec4f8ed87283108aaeddb0d659e3c5
tahakubilay <tahaayberkkubilay1864@gmail.com>
2025-11-05 16:53:53 +0300

    django admin panel ve login page için düzeltmeler

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,7 +84,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
